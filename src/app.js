37d7d9fcffd55cf98cbc77c55b81ad23b1ad9dd9
Jerick Olisea <jerickolisea1121@gmail.com>
2020-11-16 12:27:57 +0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,103 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+const app = express();
+
+//DEFINE PATHS FOR EXPRESS CONFIG
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+//SETUP HANDLEBARS ENGINE AND VIEWS LOCATION
+
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+hbs.registerPartials(partialsPath);
+
+//SETUP STATIC DIRECTORY TO SERVE
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Andrew Mead",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Emilia Clarke",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpMessage: "This is some helpful text.",
+    title: "Help",
+    name: "Andrew Mead",
+  });
+});
+//app.com
+//app.com/help
+//app.com/about
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
+    });
+  }
+
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
+
+  // res.send({
+  //   forecast: "Cloudy",
+  //   location: "Manila, Philippines",
+  //   address: req.query.address,
+  // });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Andrew Mead",
+    errorMessage: "Help article not found",
+  });
+});
+
+/////404 Page
+////always place at the bottom before app.listen
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Andrew Mead",
+    errorMessage: "Page not found",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000.");
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,65 @@
+///////// WITH OBJECT DESTRUCTURING:
+
+const request = require("request");
+
+const forecast = (lat, long, callback) => {
+  url =
+    "http://api.weatherstack.com/current?access_key=645e9aa3ff2804f1e2bbe5c46144220f&query=" +
+    encodeURIComponent(lat).toString() +
+    "," +
+    encodeURIComponent(long).toString() +
+    "&units=f";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather services.", undefined);
+    } else if (body.error) {
+      callback("Unable to find location.");
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          ". It is currently " +
+          body.current.temperature +
+          " farenheit, but it feels like " +
+          body.current.feelslike +
+          " farenheit."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
+
+//// WITHOUT OBJECT DESTRUCTURING:
+
+// const request = require("request");
+
+// const forecast = (lat, long, callback) => {
+//   url =
+//     "http://api.weatherstack.com/current?access_key=645e9aa3ff2804f1e2bbe5c46144220f&query=" +
+//     encodeURIComponent(lat).toString() +
+//     "," +
+//     encodeURIComponent(long).toString() +
+//     "&units=f";
+
+//   request({ url, json: true }, (error, response) => {
+//     if (error) {
+//       callback("Unable to connect to weather services.", undefined);
+//     } else if (response.body.error) {
+//       callback("Unable to find location.");
+//     } else {
+//       callback(
+//         undefined,
+//         response.body.current.weather_descriptions[0] +
+//           ". It is currently " +
+//           response.body.current.temperature +
+//           " farenheit, but it feels like " +
+//           response.body.current.feelslike +
+//           " farenheit."
+//       );
+//     }
+//   });
+// };
+
+// module.exports = forecast;
